feaf5eee2617faa26f474ea7e977b55ff09b98d8
nacho1785 <95112098+nacho1785@users.noreply.github.com>
2023-03-21 09:01:41 -0300

    server update

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!',
+    message: 'Life, liberty and property.',
   });
 });
 
